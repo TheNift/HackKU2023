6218d4ebac16c6d559e9e76152de2a6ba5dafec0
Ethan Tucker <ectucker1@gmail.com>
2023-04-15 17:19:26 -0500

    Finish self editing page

--- App/HackKU2023/components/SelfEditPage.tsx
+++ App/HackKU2023/components/SelfEditPage.tsx
@@ -3,7 +3,7 @@ import { List, Button } from 'react-native-paper';
 import { auth, database } from '../backend/Firebase';
 import { useAuthentication } from '../backend/useAuthentication';
 import getUid from '../backend/getUid';
-import { set, ref, onValue } from 'firebase/database';
+import { set, ref, onValue, get } from 'firebase/database';
 import VisibilityPickerComponent from './VisibilityPickerComponent';
 import StringTrait from './StringTrait'
 import { ScrollView } from 'react-native-gesture-handler';
@@ -23,7 +23,7 @@ export default function SelfEditPage(props) {
 
   const { user } = useAuthentication();
 
-  let renderTraits = (traits) => {
+  let renderTraits = (traits, setTraits) => {
     return Object.entries(traits)
       .map(([key, value]) => (
         <StringTrait
@@ -31,28 +31,44 @@ export default function SelfEditPage(props) {
           name={value.name}
           value={value.value}
           onChangeValue={(text) => {
-            let copy = {...publicTraits};
+            let copy = {...traits};
             copy[key] = { name: value.name, value: text };
-            setPublicTraits(copy);
+            setTraits(copy);
           }}
         />
       )
     );
   };
 
+  let addTrait = (traits, setTraits) => {
+    // TODO validate no duplicates
+    return (selected) => {
+      let copy = {...traits};
+      copy[selected._id] = { name: selected.value, value: '' };
+      setTraits(copy);
+    }
+  }
+
   const initDatabase = React.useEffect(() => {
-    onValue(ref(database, 'level0/' + auth.currentUser?.uid), (snapshot) => {
-      setPublicTraits(snapshot.val());
-    });
-    onValue(ref(database, 'level1/' + auth.currentUser?.uid), (snapshot) => {
-      setVillageTraits(snapshot.val());
-    });
-    onValue(ref(database, 'level2/' + auth.currentUser?.uid), (snapshot) => {
-      setGuildTraits(snapshot.val());
-    });
-    onValue(ref(database, 'level3/' + auth.currentUser?.uid), (snapshot) => {
-      setPartyTraits(snapshot.val());
-    });
+    if (Object.keys(publicTraits).length === 0 && Object.keys(villageTraits).length === 0 && Object.keys(guildTraits).length === 0 && Object.keys(partyTraits).length === 0)
+    {
+      get(ref(database, 'level0/' + auth.currentUser?.uid)).then((snapshot) => {
+        if (snapshot.exists())
+          setPublicTraits(snapshot.val());
+      });
+      get(ref(database, 'level1/' + auth.currentUser?.uid)).then((snapshot) => {
+        if (snapshot.exists())
+          setVillageTraits(snapshot.val());
+      });
+      get(ref(database, 'level2/' + auth.currentUser?.uid)).then((snapshot) => {
+        if (snapshot.exists())
+          setGuildTraits(snapshot.val());
+      });
+      get(ref(database, 'level3/' + auth.currentUser?.uid)).then((snapshot) => {
+        if (snapshot.exists())
+          setPartyTraits(snapshot.val());
+      });
+    }
   });
 
   let updateDatabase = () => {
@@ -69,27 +85,26 @@ export default function SelfEditPage(props) {
         <List.Accordion title="Public"
           expanded={publicExpanded}
           onPress={() => setPublicExpanded(!publicExpanded)}>
-          {renderTraits(publicTraits)}
-          <TextTraitPicker></TextTraitPicker>
-          <Button mode='elevated' style={styles.spacing}>Add Trait</Button>
+          {renderTraits(publicTraits, setPublicTraits)}
+          <TextTraitPicker traitPicked={addTrait(publicTraits, setPublicTraits)}/>
         </List.Accordion>
         <List.Accordion title="Village"      
           expanded={villageExpanded}
           onPress={() => setVillageExpanded(!villageExpanded)}>
-          {renderTraits(villageTraits)}
-          <Button mode='elevated' style={styles.spacing}>Add Trait</Button>
+          {renderTraits(villageTraits, setVillageTraits)}
+          <TextTraitPicker traitPicked={addTrait(villageTraits, setVillageTraits)}/>
         </List.Accordion>
         <List.Accordion title="Guild"
           expanded={guildExpanded}
           onPress={() => setGuildExpanded(!guildExpanded)}>
-          {renderTraits(guildTraits)}
-          <Button mode='elevated' style={styles.spacing}>Add Trait</Button>
+          {renderTraits(guildTraits, setGuildTraits)}
+          <TextTraitPicker traitPicked={addTrait(guildTraits, setGuildTraits)}/>
         </List.Accordion>
         <List.Accordion title="Party"
           expanded={partyExpanded}
           onPress={() => setPartyExpanded(!partyExpanded)}>
-          {renderTraits(partyTraits)}
-          <Button mode='elevated' style={styles.spacing}>Add Trait</Button>
+          {renderTraits(partyTraits, setPartyTraits)}
+          <TextTraitPicker traitPicked={addTrait(partyTraits, setPartyTraits)}/>
         </List.Accordion>
       </List.Section>
     </ScrollView>
--- App/HackKU2023/components/TextTraitPicker.tsx
+++ App/HackKU2023/components/TextTraitPicker.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
-import { SegmentedButtons } from 'react-native-paper';
+import { Button } from 'react-native-paper';
 import { PaperSelect } from 'react-native-paper-select';
 
 export default function TextTraitPicker(props) {
   const [trait, setTrait] = React.useState({
     value: 'Name',
     list: [
-      { _id: '1', value: 'Name' },
-      { _id: '2', value: 'Phone' },
-      { _id: '3', value: 'Email' },
+      { _id: 'name', value: 'Name' },
+      { _id: 'phone', value: 'Phone' },
+      { _id: 'email', value: 'Email' },
     ],
-    selectedList: [],
+    selectedList: [{ _id: 'name', value: 'Name' }],
     error: '',
   });
 
   return (
-    <View>
-      <PaperSelect
+    <View style={styles.container}>
+      <View style={styles.grow}>
+        <PaperSelect
           label="Select Trait"
           value={trait.value}
           onSelection={(value: any) => {
@@ -33,11 +34,22 @@ export default function TextTraitPicker(props) {
           multiEnable={false}
           hideSearchBox={true}
         />
+      </View>
+      <Button onPress={() => props.traitPicked(trait.selectedList[0])}>Add Trait</Button>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  grow: {
+    flexGrow: 1
+  },
   spacing: {
     marginBottom: 12
   }
