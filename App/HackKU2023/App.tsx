b1f65f2f7a46bab0a7684fd9096722df0edb1a22
Jack <jackkillschool@gmail.com>
2023-04-14 22:35:46 -0500

    Home page header

--- App/HackKU2023/App.tsx
+++ App/HackKU2023/App.tsx
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+  SafeAreaInsetsContext,
+  useSafeAreaInsets,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context';
+
+const windowDimensions = Dimensions.get('window');
+const screenDimensions = Dimensions.get('screen');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.homeHeader}>
+        <Text style={styles.headerText}>Adventurers</Text>
+      </View>
+      <ScrollView>
+        <View style={styles.adventurersWrapper}>
+          <View>
+            <Text>Questless</Text>
+          </View>
+          <View>
+            <Text>Busy Questing...</Text>
+          </View>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />  
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    display: 'flex',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  homeHeader: {
+    width: windowDimensions.width,
+    borderColor: '#000',
+    borderWidth: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    
+  },
+  headerText: {
+    color: '#000',
+    fontSize: 32,
+  },
+  adventurersWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
 });
